00298994074ba8cb9c68d36262cd00ededc4d85e
Pomelo <64297316+krumma147@users.noreply.github.com>
2023-10-04 11:40:31 +0700

    Add Game UI setting
    Homework 2/10

--- Assets/_Game/Scripts/UIManager.cs
+++ Assets/_Game/Scripts/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,11 @@ public class UIManager : MonoBehaviour
 {
     //Singleton
     public static UIManager instance;
+	[SerializeField] GameObject gameUI;
+	[SerializeField] GameObject homeUI;
+	[SerializeField] GameObject SoundSetting;
+	[SerializeField] GameObject WelcomePanel;
+	[SerializeField] Text coinTxt;
 	//   public static UIManager Instance
 	//{
 	//	get
@@ -22,9 +28,66 @@ public class UIManager : MonoBehaviour
 	private void Awake()
 	{
 		instance = this;
+		//Disable GameUI and enable HomeUI
+		DisableSoundSetting();
+		DisableWelcomePanel();
+		ShowHomeUI();
+		//When click on start button then enable Game UI and disable HomeUI (stop game play too)
 	}
 
-	[SerializeField] Text coinTxt;
+	public void StartGame()
+	{
+		EnableGameUI();
+		DisableHomeUI();
+		// Add your game start logic here
+	}
+
+	public void ShowHomeUI()
+	{
+		DisableGameUI();
+		EnableHomeUI();
+		// Add any specific logic for showing the home UI here
+	}
+
+	public void EnableWelcomePanel()
+	{
+		WelcomePanel.SetActive(true);
+	}
+
+	public void DisableWelcomePanel()
+	{
+		WelcomePanel.SetActive(false);
+	}
+
+	public void EnableSoundSetting()
+	{
+		SoundSetting.SetActive(true);
+	}
+
+	public void DisableSoundSetting()
+	{
+		SoundSetting.SetActive(false);
+	}
+
+	private void EnableHomeUI()
+	{
+		homeUI.SetActive(true);
+	}
+
+	private void DisableHomeUI()
+	{
+		homeUI.SetActive(false);
+	}
+
+	private void EnableGameUI()
+	{
+		gameUI.SetActive(true);
+	}
+
+	private void DisableGameUI()
+	{
+		gameUI.SetActive(false);
+	}
 
 	public void setCoin(int coin)
 	{
